fd07f28d1550a8bf5055b260d12e587651fbae52
Sai KUsuma C <saikusuma228@gmail.com>
2023-11-26 12:31:16 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,108 @@
+export const sample_foods =[
+    {
+        id:'1',
+        name:'Grilled Chicken',
+        Protein:'Protien: 31 grams',
+        Calories:'165 calories',
+        price: '180',
+        favorite:true,
+        stars:4.5,
+        imageUrl:'food-1.jpg',
+        tags:['Chicken','High Protein','Lunch'],
+        
+    },
+    
+    {
+        id:'2',
+        name:'Salad',
+        Protein:'Protien: 2 grams',
+        Calories:'20 calories',
+        price: '60',
+        favorite:false,
+        stars:4.6,
+        imageUrl:'food-2.jpg',
+        tags:['Salad','Low calories','Breakfast','Veg'],
+        
+    },
+    {
+        id:'3',
+        name:'Tofu Stir fry',
+        Protein:'Protien: 100 grams',
+        Calories:'144 calories',
+        price: '160',
+        favorite:false,
+        stars:4.3,
+        imageUrl:'food-3.jpg',
+        tags:['tofu','High Protein','Lunch','Veg'],
+        
+    },
+    
+    {
+        id:'4',
+        name:'Vegetable soup',
+        Protein:'Protien: 20 grams',
+        Calories:'75 calories',
+        price: '60',
+        favorite:true,
+        stars:4.8,
+        imageUrl:'food-4.jpg',
+        tags:['Soup','Veg','Starter'],
+        
+    },
+    {
+        id:'5',
+        name:'Chicken Teriyaki with brown Rice',
+        Protein:'Protien: 100 grams',
+        Calories:'400 calories',
+        price: '180',
+        favorite:true,
+        stars:4.5,
+        imageUrl:'food-5.jpg',
+        tags:['Chicken','High Protein','brown rice','Lunch'],
+        
+    },
+    
+    {
+        id:'6',
+        name:'Baked Sweet Potato',
+        Protein:'Protien: 100 grams',
+        Calories:'120 calories',
+        price: '60',
+        favorite:false,
+        stars:4.6,
+        imageUrl:'food-6.jpg',
+        tags:['Potato','Side Dish','Veg'],
+        
+    }
+];
+
+export const sample_tags=[
+    {name:'All',count:6},
+    {name:'High Protein',count:3},
+    {name:'Lunch',count:3},
+    {name:'Chicken',count:2},
+    {name:'Veg',count:4},
+    {name:'soup',count:1},
+    {name:'Starter',count:1},
+    {name:'Side Dish',count:1},
+
+]
+export const sample_users = [
+    {
+      id: 1,
+      name: 'Sai',
+      email: 'sai@gmail.com',
+      password: '12345',
+      address: 'Toronto On',
+      isAdmin: false,
+    },
+    {
+      id: 2,
+      name: 'Kusuma',
+      email: 'kusuma@gmail.com',
+      password: '12345',
+      address: 'Shanghai',
+      isAdmin: true,
+    },
+  ];
+
